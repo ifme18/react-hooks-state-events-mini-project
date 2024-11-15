7e1084be22faf643dc838c09271736222f1f22ec
ifme18 <mainakenken188@gmail.com>
2024-11-15 09:35:20 +0300

    nn

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,42 +1,13 @@
-import React, { useState } from "react";
-import CategoryFilter from "./CategoryFilter";
-import NewTaskForm from "./NewTaskForm";
-import TaskList from "./TaskList";
+import React from "react";
 
-import { CATEGORIES, TASKS } from "../data";
-
-function App() {
-  const [tasks, setTasks] = useState(TASKS);
-  const [selectedCategory, setSelectedCategory] = useState("All");
-
-  const handleDeleteTask = (taskText) => {
-    setTasks(tasks.filter(task => task.text !== taskText));
-  };
-
-  const handleAddTask = (newTask) => {
-    setTasks([...tasks, newTask]);
-  };
-
-  const handleCategoryChange = (category) => {
-    setSelectedCategory(category);
-  };
-
-  const filteredTasks = tasks.filter(task => 
-    selectedCategory === "All" || task.category === selectedCategory
-  );
-
+function Task({ text, category, onDeleteTask }) {
   return (
-    <div className="App">
-      <h2>My tasks</h2>
-      <CategoryFilter
-        categories={CATEGORIES}
-        selectedCategory={selectedCategory}
-        onCategoryChange={handleCategoryChange}
-      />
-      <NewTaskForm categories={CATEGORIES} onTaskFormSubmit={handleAddTask} />
-      <TaskList tasks={filteredTasks} onDeleteTask={handleDeleteTask} />
+    <div className="task">
+      <div className="label">{category}</div>
+      <div className="text">{text}</div>
+      <button className="delete" onClick={() => onDeleteTask(text)}>X</button>
     </div>
   );
 }
 
-export default App;
+export default Task;
